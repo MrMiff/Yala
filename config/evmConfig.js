613f116ff62f650a9f60210d5dc475d7afbab0a8
MrMiff <49294853+MrMiff@users.noreply.github.com>
2025-04-07 13:14:18 +0700

    Update evmConfig.js

--- config/evmConfig.js
+++ config/evmConfig.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+  rpcUrl: 'https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID',
+  yuTokenAddress: '0xYourYUContractAddressOnSepolia',
+  bridgeContractAddress: '0xYalaBridgeContractAddressOnSepolia',
+};
